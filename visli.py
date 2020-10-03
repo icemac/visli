b28950f9ed83dac055251ba2cee7485f2c5800c5
Michael Howitz <mh@gocept.com>
2020-10-03 21:54:20 +0200

    Implement to process multiple files.

--- visli.py
+++ visli.py
@@ -21,36 +21,38 @@ def to_hms(seconds):
 
 
 parser = argparse.ArgumentParser(usage="python visli.py", description=__doc__)
-parser.add_argument('input', action="store", help="input file")
-parser.add_argument('-d', action="store", type=int, default=15,
-                    help="duration of slices in minutes")
+parser.add_argument('input', action="store", nargs='+',
+                    help="input file(s), multiple times possible")
+parser.add_argument('-d', metavar='n', action="store", type=int, default=15,
+                    help="duration of slices in minutes, default: 15")
+
 
 args = parser.parse_args()
-input = pathlib.Path(args.input)
+input_paths = [pathlib.Path(x) for x in args.input]
 duration_in_min = args.d
 duration_hms = to_hms(duration_in_min * 60)
 
-# Taken from https://superuser.com/a/945604
-lenght_in_s = int(float(call(
-    'ffprobe',
-    '-v', 'error',
-    '-show_entries', 'format=duration',
-    '-of', 'default=noprint_wrappers=1:nokey=1',
-    input)))
 
-slice_starts = [
-    to_hms(x) for x in range(0, lenght_in_s, (duration_in_min * 60 - 30))]
-
-for index, start in enumerate(slice_starts, 1):
-    output = input.parents[0] / f'{input.stem}-{index}{input.suffix}'
-    # Taken from https://www.arj.no/2018/05/18/trimvideo/
-    call(
-        'ffmpeg',
-        '-i', input,
-        '-ss', start,
-        '-t', duration_hms,
-        '-c:v', 'copy',
-        '-c:a', 'copy',
-        output
-    )
-
+for input in input_paths:
+    # Taken from https://superuser.com/a/945604
+    lenght_in_s = int(float(call(
+        'ffprobe',
+        '-v', 'error',
+        '-show_entries', 'format=duration',
+        '-of', 'default=noprint_wrappers=1:nokey=1',
+        input)))
+    slice_starts = [
+        to_hms(x) for x in range(0, lenght_in_s, (duration_in_min * 60 - 30))]
+    print(f'Processing {input.name} ...')
+    for index, start in enumerate(slice_starts, 1):
+        output = input.parents[0] / f'{input.stem}-{index}{input.suffix}'
+        # Taken from https://www.arj.no/2018/05/18/trimvideo/
+        call(
+            'ffmpeg',
+            '-i', input,
+            '-ss', start,
+            '-t', duration_hms,
+            '-c:v', 'copy',
+            '-c:a', 'copy',
+            output
+        )
